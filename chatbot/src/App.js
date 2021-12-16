4f3610406c99220df5644dae3c2f580e835b98da
Pedro Barreto <pbdeoliveira@gmail.com>
2021-12-16 09:34:12 -0300

    editing app.js

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
+import {AccountBox} from './components/';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppContainer = styled.div`
+
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <AppContainer>
+     <AccountBox />
+     </AppContainer>
+    );
+  }
 }
 
 export default App;
