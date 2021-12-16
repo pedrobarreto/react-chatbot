71256de9594af0d80646d246ea7d0d319e97231a
Pedro Barreto <pbdeoliveira@gmail.com>
2021-12-16 16:21:26 -0300

    css fixes and starting server configs

--- chatbot/src/components/inputs.jsx
+++ chatbot/src/components/inputs.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const BoxContainer = styled.div`
-  width: 100%;
+  width: 90%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -12,6 +12,15 @@ export const FormContainer = styled.form`
   width: 100%;
   display: flex;
   flex-direction: column;
+  box-shadow: 0px 0px 2.5px rgba(15, 15, 15, 0.19);
+`;
+
+export const SignContainer = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: -100px;
+  margin-bottom: 20px;
   box-shadow: 0px 0px 2.5px rgba(15, 15, 15, 0.19);
 `;
 
@@ -31,7 +40,7 @@ export const BoldLink = styled.a`
 `;
 
 export const Input = styled.input`
-  width: 100%;
+  width: 92%;
   height: 42px;
   outline: none;
   border: 1px solid rgba(200, 200, 200, 0.3);
@@ -66,6 +75,30 @@ export const SubmitBtn = styled.button`
   transition: all, 240ms ease-in-out;
   background-image: radial-gradient( circle farthest-corner at 10% 20%,  rgba(14,174,87,1) 0%, rgba(12,116,117,1) 90% );
   );
+  
+
+  &:hover {
+    filter: brightness(1.03);
+  }
+`;
+
+export const SignBtn = styled.button`
+  width: 93%;
+  padding: 11px 40%;
+  color: #fff;
+  font-size: 15px;
+  font-weight: 600;
+  border: none;
+  border-radius: 100px 100px 100px 100px;
+  cursor: pointer;
+  transition: all, 240ms ease-in-out;
+  background: rgb(241, 196, 15);
+  background: linear-gradient(
+    58deg,
+    rgba(241, 196, 15, 1) 20%,
+    rgba(243, 172, 18, 1) 100%
+  );
+  
 
   &:hover {
     filter: brightness(1.03);
