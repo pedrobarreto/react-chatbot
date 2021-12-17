2c91b9621c68fc97ec5dd756a70dde12b3653774
Pedro Barreto <pbdeoliveira@gmail.com>
2021-12-17 17:04:08 -0300

    email validation

--- chatbot/src/components/signup.jsx
+++ chatbot/src/components/signup.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -9,14 +9,35 @@ import {
 } from "./inputs";
 import { Marginer } from './margins';
 import { AccountContext } from './context';
+import axios from 'axios';
 
 export function SignupForm(props) {
   const { switchToSignin, setUserInfo, userInfo } = useContext(AccountContext);
+  const [ dbUser, setDbUser ] = useState([])
 
   const handleChange = (e) => {
     const { name, value } = e.target
     setUserInfo({ ...userInfo, [name]: value})
   };
+
+  useEffect(()=> {
+    axios.get("http://localhost:3001/users").then((response) => {
+    setDbUser(response.data);
+    })
+  },[])
+ 
+
+  const emailValidation = () => {
+   const emailInDb = dbUser.some(({email}) => email === userInfo.email );
+   const validEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(emailInDb){
+      console.log('email já cadastrado');
+    } else if ( userInfo.email.match(validEmail)){
+      console.log('email válido');
+    } else {
+      console.log('email inválido');  
+    }        
+  }
   
   return (
     <BoxContainer>
@@ -27,7 +48,7 @@ export function SignupForm(props) {
         <Input type="password" name= "checkpassword" onChange={handleChange}  placeholder="Confirmar Senha" />
       </SignContainer>
       <Marginer direction="vertical" margin={10} />
-      <SignBtn type="submit" onClick={switchToSignin} >Enviar </SignBtn>
+      <SignBtn type="submit" onClick={emailValidation} >Enviar </SignBtn>
       <Marginer direction="vertical" margin="1em" />
       <Link href="#">
         Já possui uma conta ? 
