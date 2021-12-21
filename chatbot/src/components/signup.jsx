bfc5831eb9e1fe9d59c91a670e86de9be49a3a63
Pedro Barreto <pbdeoliveira@gmail.com>
2021-12-21 12:00:05 -0300

    sign up validation using react hook form

--- chatbot/src/components/signup.jsx
+++ chatbot/src/components/signup.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -10,10 +10,16 @@ import {
 import { Marginer } from './margins';
 import { AccountContext } from './context';
 import axios from 'axios';
+import { useForm } from 'react-hook-form';
+
 
 export function SignupForm(props) {
   const { switchToSignin, setUserInfo, userInfo } = useContext(AccountContext);
-  const [ dbUser, setDbUser ] = useState([])
+  const { register, errors, handleSubmit, watch } = useForm({});
+  const [ dbUser, setDbUser ] = useState([]);
+  const emailInDb = dbUser.some(({email}) => email === userInfo.email );
+  const password = useRef({});
+  password.current = watch("password", "");
 
   const handleChange = (e) => {
     const { name, value } = e.target
@@ -25,37 +31,73 @@ export function SignupForm(props) {
     setDbUser(response.data);
     })
   },[])
- 
-
-  const handleSubmit = () => {
-   const emailInDb = dbUser.some(({email}) => email === userInfo.email );
-   const validEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if ( userInfo.name && userInfo.name.length < 3){
-    alert('Preencha com um nome válido') 
-    } else if( emailInDb){
-      alert('email já cadastrado')
-    } else if ( userInfo.email && !userInfo.email.match(validEmail)){
-      alert('email inválido')
-    } else if ( userInfo.password && !userInfo.password.match(userInfo.checkpassword) ) {
-      alert('A senha não confere')
-    } else if ( userInfo.name && userInfo.email.match(validEmail) && userInfo.password ) {
-      console.log('bbb');
-      return switchToSignin()
-    } else {
-      alert('erro')
-    }
-  }
   
   return (
     <BoxContainer>
       <SignContainer>
-        <Input type="text" name="name" onChange={handleChange} placeholder="Nome Completo" />
-        <Input type="email" name="email" onChange={handleChange} placeholder="Email" />
-        <Input type="password" name="password" onChange={handleChange} placeholder="Senha" />
-        <Input type="password" name= "checkpassword" onChange={handleChange}  placeholder="Confirmar Senha" />
-      </SignContainer>
+            <Input
+        onChange={handleChange}
+        placeholder="Nome Completo"
+        name="name"
+        type="text"
+        ref={register({
+          required: "Campo obrigatório",
+          minLength: {
+            value: 2,
+            message: "Por favor preencha um nome válido"
+          }
+        })}
+      />
+        <form onSubmit={e => e.preventDefault()}>
+        <Input
+         onChange={handleChange}
+          type="email"
+          id="inputEmail"
+          name="email"
+          placeholder="Email"
+          ref={register({
+            validate: {
+              isEmail: () =>
+              !emailInDb || "Email já cadastrado 📧",
+            },
+            required: "Email obrigatório",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "Digite um email válido",
+            },
+          })}
+        />
+        {errors.email && <p className="error">{errors.email.message}</p>}
+        <Input
+        onChange={handleChange}
+        placeholder="Senha"
+        name="password"
+        type="password"
+        ref={register({
+          required: "Campo obrigatório",
+          minLength: {
+            value: 4,
+            message: "A senha deve ser maior que 4 caracteres"
+          }
+        })}
+      />
+      {errors.password && <p>{errors.password.message}</p>}
+
+      <Input
+        name= "checkpassword"
+        placeholder="Confirmar Senha"
+        onChange={handleChange}
+        type="password"
+        ref={register({
+          validate: value =>
+            value === password.current || "As senhas não são iguais."
+        })}
+      />
+      {errors.password_repeat && <p>{errors.password_repeat.message}</p>}
       <Marginer direction="vertical" margin={10} />
-      <SignBtn type="submit" onClick={handleSubmit} >Enviar </SignBtn>
+        <SignBtn type="submit" onClick={handleSubmit(switchToSignin)}> Enviar </SignBtn>
+        </form>
+      </SignContainer>
       <Marginer direction="vertical" margin="1em" />
       <Link href="#">
         Já possui uma conta ? 
